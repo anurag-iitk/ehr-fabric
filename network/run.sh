ad89360e28cc4e0e7464de4d0d5721f59309c051
anurag-iitk <anurag@c3ihub.iitk.ac.in>
2023-10-14 00:33:02 +0530

    refactor run script

--- network/run.sh
+++ network/run.sh
@@ -1,195 +1,102 @@
 
 
 export MSYS_NO_PATHCONV=1
+export VERBOSE=false
 ORDERER_CA=/opt/src/github.com/hyperledger/ehr-fabric/network/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 docker-compose -f docker-compose.yaml  up -d 2>&1
 docker-compose -f docker-compose.yaml up -d
 docker ps -a
 
-export PATH=$GOPATH/src/github.com/hyperledger/ehr-fabric/bin:${PWD}/../bin:${PWD}:$PATH
+ROOTDIR=$(cd "$(dirname "$0")" && pwd)
+export PATH=${ROOTDIR}/../bin:${PWD}/../bin:$PATH
 export FABRIC_CFG_PATH=${PWD}
 
-FIRST_CHANNEL_NAME=commonChannel
-SECOND_CHANNEL_NAME=Org1AndOrg2Channel
-THIRD_CHANNEL_NAME=Org2AndOrg4Channel
-
-SYS_FIRST_CHANNEL=first-system-channel
-SYS_SECOND_CHANNEL=second-system-channel
-SYS_THIRD_CHANNEL=third-system-channel
 # remove previous crypto material and config transactions
 rm -Rf channel-artifacts/*
 rm -Rf organizations/*
 
-echo "Generate certificates using cryptogen tool"
-
+echo "######################################################################################################################################################"
+echo ""################################################## Generate certificates using cryptogen tool "##################################################"
+echo "######################################################################################################################################################"
 # ../bin/cryptogen generate --config=crypto-config.yaml --output="crypto-config" 
-
-# generate crypto material
 cryptogen generate --config=./crypto-config.yaml
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate crypto material..."
   exit 1
 fi
 
-sleep 2
-
-
 ./ccp-generate.sh
 
+generateChannel() {
+  ordererProfileName="$1"
+  systemChannelName="$2"
+  channelName="$3"
+  channelProfileName="$4"
 
-echo "################################# CHANNEL ONE #######################################"
-sleep 3
-echo "=============== Generating Genesis block ==================="
-sleep 2
-generate genesis block for orderer
-configtxgen -profile FourOrgsOrdererGenesis -channelID $SYS_FIRST_CHANNEL -outputBlock ./channel-artifacts/genesis.block
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate orderer genesis block..."
-  exit 1
-fi
+  echo "######################################################################################################################################################"
+  echo "################################################## Generating Genesis block ##################################################"
+  echo "######################################################################################################################################################"
+  configtxgen -profile $ordererProfileName -channelID $systemChannelName -outputBlock ./$channelName/genesis.block
+  sleep 2
+  echo "######################################################################################################################################################"
+  echo "################################################## Generating channel configuration transaction ##################################################"
+  echo "######################################################################################################################################################"
+  configtxgen -profile $channelProfileName -outputCreateChannelTx ./$channelName/channel.tx -channelID $channelName
+  sleep 2
+}
 
-echo "=================== Generating channel configuration transaction ========================="
-sleep 2
-set -x
-configtxgen -profile FourOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $FIRST_CHANNEL_NAME
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate channel configuration transaction..."
-  exit 1
-fi
-
-echo "======================== Generating anchor peer update for Org1MSP ================================="
-sleep 2
-set -x
-configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $FIRST_CHANNEL_NAME -asOrg Org1MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update for Org1MSP..."
-  exit 1
-fi
-
-echo "======================== Generating anchor peer update for Org2MSP ================================="
-sleep 2
-set -x
-configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $FIRST_CHANNEL_NAME -asOrg Org2MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update for Org2MSP..."
-  exit 1
-fi
-
-echo "======================== Generating anchor peer update for Org3MSP ================================="
-sleep 2
-set -x
-configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org3MSPanchors.tx -channelID $FIRST_CHANNEL_NAME -asOrg Org3MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update for Org3MSP..."
-  exit 1
-fi
-
-echo "======================== Generating anchor peer update for Org4MSP ================================="
-sleep 2
-set -x
-configtxgen -profile FourOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org4MSPanchors.tx -channelID $FIRST_CHANNEL_NAME -asOrg Org4MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update for Org4MSP..."
-  exit 1
-fi
-
-# sleep 5
-
-# create a channel
-# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/src/github.com/hyperledger/ehr-fabric/network/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer0.org1.example.com peer  channel create -o orderer.example.com:7050 -c mychannel -f /channel-artifacts/channel.tx --tls true --cafile /opt/src/github.com/hyperledger/ehr-fabric/network/crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-# peer0.org3
-# docker exec -e "CORE_PEER_LOCALMSPID=Org3MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/src/github.com/hyperledger/ehr-fabric/network/crypto-config/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp" peer0.org3.example.com peer channel join -b mychannel.block
+generateAnchorPeers() {
+  channelName="$1"
+  orgMSP="$2"
+  profileName="$3"
+  echo "######################################################################################################################################################"
+  echo "################################################## Generating anchor peer update for $orgMSP in channel $channelName ##################################################"
+  echo "######################################################################################################################################################"
+  sleep 2
+  set -x
+  configtxgen -profile $profileName -outputAnchorPeersUpdate "./$channelName/$orgMSP-anchors.tx" -channelID "$channelName" -asOrg "$orgMSP"
+  if [ "$?" -ne 0 ]; then
+FIRST_CHANNEL_NAME=CommonChannel
+    echo "Failed to generate anchor peer update for $orgMSP in channel $channelName..."
+    exit 1
+  fi
+  echo "Anchor peer update for $orgMSP generated successfully in channel $channelName."
+}
 
 
-
-echo "################################# SECOND CHANNEL #######################################"
-sleep 3
-
-
-echo "=============== Generating Second channel Genesis block ==================="
-sleep 2
-
-generate genesis block for orderer
-configtxgen -profile TwoOrgsOrdererGenesis -channelID $SYS_SECOND_CHANNEL -outputBlock ./channel-artifacts-2/genesis.block
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate second orderer genesis block..."
-  exit 1
-fi
-
-echo "=================== Generating Second channel configuration transaction ========================="
-sleep 2
-set -x
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts-2/channel.tx -channelID $SECOND_CHANNEL_NAME
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate second channel configuration transaction..."
-  exit 1
-fi
-
-echo "======================== Generating Second channel anchor peer update for Org1MSP ================================="
-sleep 2
-set -x
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts-2/Org1MSPanchors.tx -channelID $SECOND_CHANNEL_NAME -asOrg Org1MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update second channel for Org1MSP..."
-  exit 1
-fi
-
-echo "======================== Generating Second channel anchor peer update for Org2MSP ================================="
-sleep 2
-set -x
-configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts-2/Org2MSPanchors.tx -channelID $SECOND_CHANNEL_NAME -asOrg Org2MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update second channel for Org2MSP..."
-  exit 1
-fi
-
-
-echo "################################# CHANNEL THIRD #######################################"
+echo "######################################################################################################################################################"
+echo "################################################## CHANNEL ONE - COMMON CHANNEL ##################################################"
+echo "######################################################################################################################################################"
 sleep 5
+generateChannel FourOrgsOrdererGenesis firstSystemChannel CommonChannel FourOrgsChannel  
+orgMSPArray=("Org1MSP" "Org2MSP" "Org3MSP" "Org4MSP")
+# Generate anchor peer updates for each org
+for orgMSP in "${orgMSPArray[@]}"; do
+  generateAnchorPeers CommonChannel "$orgMSP" FourOrgsChannel
+done
 
 
-echo "=============== Generating Third channel Genesis block ==================="
-sleep 2
+echo "######################################################################################################################################################"
+echo "################################################## CHANNEL SECOND B/W HOSPITAL AND DOCTOR ##################################################"
+echo "######################################################################################################################################################"
+sleep 5
+generateChannel TwoOrgsOrdererGenesis secondSystemChannel Org1AndOrg2Channel TwoOrgsChannel  
+orgMSPArray=("Org1MSP" "Org2MSP")
+# Generate anchor peer updates for each org
+for orgMSP in "${orgMSPArray[@]}"; do
+  generateAnchorPeers Org1AndOrg2Channel "$orgMSP" TwoOrgsChannel
+done
 
-generate genesis block for orderer
-configtxgen -profile TwoOrgOrdererGenesis -channelID $SYS_THIRD_CHANNEL -outputBlock ./channel-artifacts-3/genesis.block
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate third orderer genesis block..."
-  exit 1
-fi
 
-echo "=================== Generating Third channel configuration transaction ========================="
-sleep 2
-set -x
-configtxgen -profile TwoOrgChannel -outputCreateChannelTx ./channel-artifacts-3/channel.tx -channelID $THIRD_CHANNEL_NAME
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate third channel configuration transaction..."
-  exit 1
-fi
+echo "######################################################################################################################################################"
+echo "################################################## CHANNEL THIRD B/W DOCTOR AND PATIENT ##################################################"
+echo "######################################################################################################################################################"
+sleep 5
+generateChannel TwoOrgOrdererGenesis thirdSystemChannel Org2AndOrg4Channel TwoOrgChannel  
+orgMSPArray=("Org2MSP" "Org4MSP")
+# Generate anchor peer updates for each org
+for orgMSP in "${orgMSPArray[@]}"; do
+  generateAnchorPeers Org2AndOrg4Channel "$orgMSP" TwoOrgChannel
+done
 
-echo "======================== Generating Third channel anchor peer update for Org2MSP ================================="
-sleep 2
-set -x
-configtxgen -profile TwoOrgChannel -outputAnchorPeersUpdate ./channel-artifacts-3/Org2MSPanchors.tx -channelID $THIRD_CHANNEL_NAME -asOrg Org2MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update third channel for Org2MSP..."
-  exit 1
-fi
-
-echo "======================== Generating Third channel anchor peer update for Org4MSP ================================="
-sleep 2
-set -x
-configtxgen -profile TwoOrgChannel -outputAnchorPeersUpdate ./channel-artifacts-3/Org4MSPanchors.tx -channelID $SECOND_CHANNEL_NAME -asOrg Org4MSP
-
-if [ "$?" -ne 0 ]; then
-  echo "Failed to generate anchor peer update third channel for Org4MSP..."
-  exit 1
-fi
